e1f5d27d08e47a33039ed7c5232907bd7e8100e3
XoX-02 <boy3898@naver.com>
2026-03-13 12:34:03 +0900

    update

--- code/4-2.c
+++ code/4-2.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-int main() {
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            printf("%2d x %2d = %2d\n ", i, j, i * j);
+#include <stdio.h> 
+int main() { 
+    for (int i = 1; i <= 9; i++) { 
+        for (int j = 1; j <= 9; j++) { 
+            printf("%2d x %2d = %2d\n", i, j, i * j); 
         }
-    }
-            return 0;
+    } 
+            return 0; 
 }
